40ab0ae24f0d5e34c36853e2d9754c5b5ef31708
Riccardo <rcr.ragona@gmail.com>
2021-04-15 09:35:36 +0200

    Added V calib

--- TWAdata/TWAdata.py
+++ TWAdata/TWAdata.py
@@ -56,6 +56,8 @@ class TWAdata():
             self._df['time'] = time_relative
             self._df.set_index('time', inplace=True)
 
+            # TODO add frequency
+            self.fMHz = 50
             # TODO post processing
             self.raw_TOS_to_TOS()
 
@@ -143,4 +145,118 @@ class TWAdata():
         '''
         self._def['TOS'] = 32.824 * self._def['TOS_raw'] - 29.717
 
+    def AD8310_calib(self,card,Vdc):
+        '''
+        provides the transformation Vdc => dBm for the AD8310 cards
+
+        Parameters
+        ----------
+        card : srt
+            one of the calibrated cards.
+        Vdc : float
+            measured voltage.
+
+        Returns
+        -------
+        dBm : float
+            the Vdc value in dBm
+
+        '''
+        cards={'V1':{'a':41.145,'b':-91.911},
+               'V2':{'a':40.699,'b':-91.764},
+               'V3':{'a':40.751,'b':-90.550},
+               'V4':{'a':41.516,'b':-93.958},
+               'V5':{'a':40.798,'b':-93.086},
+               'V6':{'a':41.110,'b':-93.305},
+               'V7':{'a':40.737,'b':-92.731},}
         
+        return cards[card]['a'] * Vdc + cards[card]['b']
+    
+    def V_probe_calib(self,probe,fMHz):
+        '''
+        provides the calibrated coefficient of the voltage probe
+        expressed in kV/V.
+
+        Parameters
+        ----------
+        probe : str
+            one of the calibrated probes.
+        fMHz : float
+            the frequency in [MHz].
+
+        Returns
+        -------
+        kV_V : float
+            the voltage probe coefficient
+
+        '''
+        probes = {'V1':297.662,
+                  'V2':293.318,
+                  'V3':245.413,
+                  'V4':325.128,
+                  'V5':320.402,
+                  'V6':283.714,}
+        
+        return probes[probe]/fMHz
+    
+    def dBm_to_Vrf(self,dBm):
+        '''
+        transform dBm in Vrf
+
+        Parameters
+        ----------
+        dBm : float
+            value in [dBm].
+
+        Returns
+        -------
+        Vrf : float
+            value in [V]
+
+        '''
+        return 10**((dBm-13)/20)*np.sqrt(2)
+    
+    def raw_V_to_V(self):
+        '''
+        
+
+        Returns
+        -------
+        None.
+
+        '''
+        card_stage = self.AD8310_calib('V1',self._df['V1_raw'])
+        cable_stage = self.cable_calib('cable_1', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V1', self.fMHz)
+        self._df['V1'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V2',self._df['V2_raw'])
+        cable_stage = self.cable_calib('cable_2', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V2', self.fMHz)
+        self._df['V2'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V3',self._df['V3_raw'])
+        cable_stage = self.cable_calib('cable_3', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V3', self.fMHz)
+        self._df['V3'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V3',self._df['V3_raw'])
+        cable_stage = self.cable_calib('cable_3', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V3', self.fMHz)
+        self._df['V3'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V4',self._df['V4_raw'])
+        cable_stage = self.cable_calib('cable_4', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V4', self.fMHz)
+        self._df['V4'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V5',self._df['V5_raw'])
+        cable_stage = self.cable_calib('cable_5', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V5', self.fMHz)
+        self._df['V5'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+        
+        card_stage = self.AD8310_calib('V6',self._df['V6_raw'])
+        cable_stage = self.cable_calib('cable_6', self.fMHz)[0]
+        probe_coeff = self.V_probe_calib('V6', self.fMHz)
+        self._df['V6'] = self.dBm_to_Vrf(card_stage + cable_stage + 20) * probe_coeff
+    
