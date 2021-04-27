56beef23c2f9e78ea19c2bbe595cc3ebfb8c9066
Julien Hillairet <julien.hillairet@gmail.com>
2021-04-27 16:01:15 +0200

    27/04/2021 Logbook

---
+++ 2021-04-27_RF_commissioning.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 16:43:05 2021
+
+@author: JH218595
+"""
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from TWAdata.TWAdata import TWAdata
+#%%
+filename='TWA_2021-04-27_13-39-24_UTC_F55,500000M'
+tdms_filename = 'data/2021-04-27_RF_Commissioning/'+filename+'.tdms'
+
+data = TWAdata(tdms_filename)
+
+# title for graphs
+title = np.datetime_as_string(data._raw_data['start_time'])+' - '+str(data.fMHz)+'MHz'
+
+##%%
+
+#full window
+#mng = plt.get_current_fig_manager()
+#mng.frame.Maximize(True)
+
+# plot something
+fig, ax = plt.subplots(7,1, sharex=True)
+ax[0].set_title(title)
+
+data.df.plot(y='V1', ax=ax[0])
+data.df.plot(y='V2', ax=ax[0])
+data.df.plot(y='V3', ax=ax[0])
+data.df.plot(y='V4', ax=ax[0])
+data.df.plot(y='V5', ax=ax[0])
+data.df.plot(y='V6', ax=ax[0])
+
+data.df.plot(y='RL', ax=ax[1], label='RL')
+data.df.plot(y='RLG', ax=ax[1], label='RLGen')
+
+data.df['Ptg'] = data.df['Pig']-data.df['Prg']
+data.df.plot(y='Pig', ax=ax[2])
+data.df.plot(y='Piout', ax=ax[2])
+data.df.plot(y='Ptg', ax=ax[2])
+data.df.plot(y='Piin', ax=ax[2])
+
+data.df.plot(y='Prg', ax=ax[3])
+
+data.df.plot(y='Vac1', ax=ax[4])
+data.df.plot(y='Vac2', ax=ax[4])
+
+data.df.plot(y='TC1', ax=ax[5])
+data.df.plot(y='TC2', ax=ax[5])
+data.df.plot(y='TC3', ax=ax[5])
+
+data.df.plot(y='Pm1', ax=ax[6])
+data.df.plot(y='Pm2', ax=ax[6])
+data.df.plot(y='Pm3', ax=ax[6])
+data.df.plot(y='Pm4', ax=ax[6])
+data.df.plot(y='Pm5', ax=ax[6])
+data.df.plot(y='Pm6', ax=ax[6])
+
+ax[4].set_yscale('log')
---
+++ data/2021-04-27_RF_Commissioning/README.md
@@ -0,0 +1,24 @@
+# 2021-04-27 - TWA mockup RF Tests
+
+| Time | Pin/duration | fMHz | Comment | 
+| :--: | :----------: | :--: | :-----: |
+| 13-06-15 | 1.00 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-10-21 | 1.20 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-11-53 | 1.40 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-12-55 | 1.60 MW / 3s | 55.5 | 2s ramp-up -> SHAD triggers | 
+| 13-15-00 | 1.50 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-15-35 | 1.60 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-16-27 | 1.70 MW / 3s | 55.5 | 2s ramp-up -> OK | 
+| 13-17-21 | 1.75 MW / 3s | 55.5 | 2s ramp-up -> SHAD triggers | 
+| 13-18-08 | 1.75 MW / 3s | 55.5 | repeat -> SHAD triggers |
+| 13-19-18 | 1.75 MW / 3s | 55.5 | repeat changing SHAD channel -> SHAD triggers all the time (?) |
+| 13-21-09 | 1.75 MW / 3s | 55.5 | repeat -> SHAD triggers |
+| 13-21-58 | 1.75 MW / 3s | 55.5 | repeat without SHAD -> OK |
+| 13-24-17 | 1.80 MW / 3s | 55.5 | repeat without SHAD ->  generator limitations (1.2 MW dissipated power limit reached) |
+| 13-25-54 | 1.80 MW / 3s | 55.5 | repeat without SHAD ->  generator limitations (1.2 MW dissipated power limit reached) |
+| 13-30-50 | 1.80 MW / 3s | 55.5 | repeat without SHAD and reduction factor 0.8 to 0.6 and 1.3 MW dissipated power -> 2 generator limit |
+| 13-34-47 | 1.90 MW / 2s | 55.5 | repeat with shorter plateau length -> still some generator limits reached |
+| 13-36-52 | 1.90 MW / 2s | 55.5 | repeat with 1.4 MW on dissipated power -> OK ! |
+| 13-39-24 | 1.90 MW / 3s | 55.5 | repeat a bit longer -> OK ! |
+
+
